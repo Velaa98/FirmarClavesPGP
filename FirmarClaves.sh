2150f9f2d886706a189acc6b84878971d21a27bd
José Manuel Vela <vela.1998@gmail.com>
2019-01-25 10:52:13 +0100

    use for instead of while

--- FirmarClaves.sh
+++ FirmarClaves.sh
@@ -1,6 +1,11 @@
 #!/usr/env/env bash
 
-while IFS=\/n read KeyID
+#while IFS=\/n read KeyID
+#do
+#	gpg --quick-sign-key $KeyID
+#	gpg --keyserver pgp.rediris.es --send-keys $KeyID
+#done < claves.txt
+for KeyID in $(cat prueba)
 do
 	gpg --quick-sign-key $KeyID
 	gpg --keyserver pgp.rediris.es --send-keys $KeyID
