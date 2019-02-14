2192e88586d5af7e37f065587a868521e8253eb1
José Manuel Vela Fernández <vela.1998@gmail.com>
2019-02-14 10:33:31 +0100

    Update FirmarClaves.sh

--- FirmarClaves.sh
+++ FirmarClaves.sh
@@ -5,8 +5,8 @@
 #	gpg --quick-sign-key $KeyID
 #	gpg --keyserver pgp.rediris.es --send-keys $KeyID
 #done < claves.txt
-for KeyID in $(cat prueba)
+for KeyID in $(cat claves.txt)
 do
 	gpg --quick-sign-key $KeyID
 	gpg --keyserver pgp.rediris.es --send-keys $KeyID
-done < claves.txt
+done
